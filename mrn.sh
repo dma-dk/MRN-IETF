eb117d5b33c5303332aff10ce9405394a5d4c7f1
Kasper Nielsen <kasperni@gmail.com>
2016-11-23 16:48:02 +0100

    stuff

--- mrn.sh
+++ mrn.sh
@@ -1,9 +1,19 @@
-!/bin/bash
+#!/bin/bash
 
 if [ "$1" = "pdf" ]; then
    cd adoc
    asciidoctor-pdf -r asciidoctor-diagram identity.adoc -D ../target -a imagesdir=../images/
    cd ..
+   exit
+   
+elif [ "$1" = "mmark" ]; then   
+   rm -rf target/ 
+   mkdir target
+
+   ./mrn/mmark -xml2 -page mrn/mrn.md > target/x.xml 
+   xml2rfc --text target/x.xml
+   rm target/x.xml
+   mv target/x.txt target/mrn.txt
    exit
 elif [ "$1" = "html" ]; then
    asciidoctor -r asciidoctor-diagram website/site.adoc -D target
@@ -17,14 +27,23 @@ elif [ "$1" = "site" ]; then
    fi
    rm -rf target/
    asciidoctor -r asciidoctor-diagram website/site.adoc -D target
+
+   ./mrn/mmark -xml2 -page mrn/mrn.md > target/x.xml 
+   xml2rfc --text target/x.xml
+   rm target/x.xml
+   mv target/x.txt target/mrn.txt
+   
 #   mkdir -p target/images
 #   cp images/*.png target/images/
    
    git checkout gh-pages
    cp target/site.html index.html
+   cp target/mrn.txt mrn.txt
 #   mkdir -p ../identity/images
 #   cp target/images/* ../identity/images/
    git add index.html
+   git add mrn.txt
+
 #   git add ../identity/images/*
    git commit -m "update documentation"
    git push
